8faae4e168caa27c647e220e65b108d34a2fe6a6
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-05-26 17:59:26 +0300

    more-or-less stable

--- server.js
+++ server.js
@@ -38,14 +38,15 @@ opts.HOSTNAME = 'okupatsioon.entu.ee'
 var HOME_DIR = path.dirname(process.argv[1])
 var TEMP_DIR = path.resolve(HOME_DIR, 'temp')
 var EntuLib = entulib(opts.USER_ID, opts.API_KEY, opts.HOSTNAME)
-var LIMIT = 5
-var result_counter = 0
+var PAGE_SIZE_LIMIT = 10
 var connection_counter = 0
+var download_counter = 0
+var MAX_DOWNLOAD_COUNT = 15
 
 var fetchNextPage = function fetchNextPage(page) {
 
     connection_counter ++
-    EntuLib.findEntity('eksponaat', '00000', LIMIT, page, function findEntityCB(err, result) {
+    EntuLib.findEntity('eksponaat', '0000', PAGE_SIZE_LIMIT, page, function findEntityCB(err, result) {
         connection_counter --
         if (err) {
             console.log('Can\'t reach Entu', err, result)
@@ -56,11 +57,9 @@ var fetchNextPage = function fetchNextPage(page) {
         } else {
 
             console.log('Fetched ' + result.result.length + '/' + result.count
-                + ' results on page ' + page + '/' + Math.ceil(result.count / LIMIT))
+                + ' results on page ' + page + '/' + Math.ceil(result.count / PAGE_SIZE_LIMIT))
             // console.log(result.result)
             result.result.forEach(function entityLoop(entity) {
-                result_counter ++
-                // console.log(result_counter + ':', entity.id)
 
                 connection_counter ++
                 EntuLib.getEntity(entity.id, function findEntityCB(err, result) {
@@ -75,13 +74,14 @@ var fetchNextPage = function fetchNextPage(page) {
                     } else {
                         // console.log(entity.id + ':', result.result.displayname, result.result.displayinfo)
                         var photo_property = result.result.properties['photo']
-                        var code_property = result.result.properties['code']
+                        var code_value = result.result.properties['code'].values ? result.result.properties['code'].values[0].value : ''
+                        var nimetus_value = result.result.properties['tag'].values ? result.result.properties['tag'].values[0].value : ''
                         if (photo_property.values) {
                             photo_property.values.forEach(function photoLoop(photo_val) {
-                                console.log(entity.id + '/' + photo_val.id + '[' + photo_val.db_value + ']:', photo_val.value)
-                                var ff = new fetchFile(photo_val.db_value, photo_val.value, code_property.values[0].value)
+                                // console.log(entity.id + '/' + photo_val.id + '[' + photo_val.db_value + ']:', photo_val.value)
+                                var ff = new fetchFile(photo_val.db_value, photo_val.value, code_value, nimetus_value)
                                 .on('error', function fileFetchError(err_msg, err_no) {
-                                    console.log(err_msg, err_no)
+                                    console.log('fetchFile error: ', err_msg, err_no)
                                     process.exit(err_no)
                                 })
                             })
@@ -90,8 +90,17 @@ var fetchNextPage = function fetchNextPage(page) {
                 })
             })
 
-            if (LIMIT * page < result.count) {
-                fetchNextPage(page+1)
+            if (PAGE_SIZE_LIMIT * page < result.count) {
+                console.log('Currently connections: #' + connection_counter + '; downloads: #' + download_counter)
+                var fetchIfReady = function fetchIfReady(page) {
+                    if (download_counter < MAX_DOWNLOAD_COUNT) {
+                        fetchNextPage(page)
+                    } else {
+                        console.log('Postponing load of page #' + page)
+                        setTimeout(function() { fetchIfReady(page) }, 1000)
+                    }
+                }
+                fetchIfReady(page + 1)
             } else {
                 console.log('No more pages for today.')
                 // process.exit(0)
@@ -105,85 +114,98 @@ var fetchNextPage = function fetchNextPage(page) {
 
 fetchNextPage(1)
 
-var max_download_time = 30 // seconds
-var loading_process_count = 0
+var MAX_DOWNLOAD_TIME = 5 * 60 // seconds
 var total_download_size = 0
 var bytes_downloaded = 0
 var decrementProcessCount = function decrementProcessCount() {
-    -- loading_process_count
+    -- download_counter
 }
 var incrementProcessCount = function incrementProcessCount() {
-    ++ loading_process_count
+    ++ download_counter
 }
 var countLoadingProcesses = function countLoadingProcesses() {
-    return loading_process_count
+    return download_counter
 }
 
 var append_background = path.resolve(HOME_DIR, 'text_background.png')
+var error_stream = fs.createWriteStream('errors.txt')
 
-var fetchFile = function fetchFile(file_id, file_name, exp_nr) {
+var fetchFile = function fetchFile(file_id, file_name, exp_nr, nimetus) {
+    incrementProcessCount()
     EventEmitter.call(this)
     var self = this
 
     var t = setTimeout(function() {
         decrementProcessCount()
-        console.log('Downloading: ' + file_name + ' - ' + helper.bytesToSize(bytes_downloaded) + ' of ' + helper.bytesToSize(total_download_size))
-        self.emit('error', file_id + '|' + file_name + ' timed out!', 95)
-    }, max_download_time * 1000)
+        self.emit('error', 'Error: ' + file_id + '|' + fetch_uri + '|' + file_name + ' timed out!', 95)
+    }, MAX_DOWNLOAD_TIME * 1000)
 
     var fetch_uri = 'https://' + opts.HOSTNAME + '/api2/file-' + file_id
     var download_filename = path.resolve(TEMP_DIR, file_id + path.extname(file_name))
-    console.log(fetch_uri + '-->' + download_filename)
+    // console.log(fetch_uri + '-->' + download_filename)
 
-    incrementProcessCount()
     // var resize = im().resize('800x600').quality(90)
     var resize = gm().resize('800x600').append('./text_background.png')
 
     gm(request
         .get(fetch_uri)
         .on('error', function(err) {
-            console.log(err)
+            console.log('request error for: ' + file_id + '|' + fetch_uri + '|' + file_name, err)
+            decrementProcessCount()
+            fetchFile(file_id, file_name, exp_nr, nimetus)
         })
         .on('response', function response_handler( response ) {
             var filesize = response.headers['content-length']
             // console.log(response.headers)
-            if (filesize) {
-                console.log(file_id + '|' + fetch_uri + '|' + file_name + ' has no size!')
+            if (filesize === undefined) {
+                console.log(file_id + '|' + fetch_uri + '|' + file_name + ' has no size! Concurrent downloads: #' + download_counter)
+                decrementProcessCount()
+                setTimeout(function() {
+                    fetchFile(file_id, file_name, exp_nr, nimetus)
+                }, 1000)
+                return
                 // self.emit('error', file_id + '|' + fetch_uri + '|' + file_name + ' has no size!', 90)
                 // return
+            } else {
                 total_download_size += Number(filesize)
-                // console.log('Downloading: ' + file_name + ' - ' + helper.bytesToSize(bytes_downloaded) + ' of ' + helper.bytesToSize(total_download_size))
+                // console.log('Downloading: ' + file_id + '|' + fetch_uri + '|' + file_name + ' - ' + filesize + ' bytes.')
             }
             response.on('data', function(chunk) {
                 bytes_downloaded += chunk.length
                 if (filesize === undefined) {
+                    console.log(file_id + '|' + fetch_uri + '|' + file_name + ' loaded chunk of ' + chunk.length + ' bytes.')
                     total_download_size += chunk.length
+                    console.log(chunk.toString('utf8'))
                 }
                 // console.log('Progress: ' + file_name + ' - ' + helper.bytesToSize(total_download_size) + ' - ' + helper.bytesToSize(bytes_downloaded) + ' = ' + helper.bytesToSize(total_download_size - bytes_downloaded) )
             })
             response.on('end', function() {
                 if (response.statusCode === 200) {
-                    console.log('Finished: ' + file_name + ' - ' + helper.bytesToSize(total_download_size) + ' - ' + helper.bytesToSize(bytes_downloaded) + ' = ' + helper.bytesToSize(total_download_size - bytes_downloaded) )
-                    decrementProcessCount()
-                } else {
-                    decrementProcessCount()
+                    // console.log('Finished: ' + file_id + '|' + fetch_uri + '|' + file_name + ' - ' + helper.bytesToSize(total_download_size) + ' - ' + helper.bytesToSize(bytes_downloaded) + ' = ' + helper.bytesToSize(total_download_size - bytes_downloaded) )
                 }
+                decrementProcessCount()
                 clearTimeout(t)
             })
         })
     )
-    .resize(800, 550)
-    .append(append_background)
-    .drawText(10, 20, 'Okupatsioonide Muuseumi eksponaat #' + exp_nr ,'south')
+    .resize(800, 530)
+    // .append(append_background)
+    // .drawText(10, 20, 'Okupatsioonide Muuseumi eksponaat #' + exp_nr, 'south')
     .stream(function(err, stdout, stderr) {
-        var f = fs.createWriteStream(download_filename)
-        var e = fs.createWriteStream(download_filename + '.output.txt')
-
-        stderr.pipe(e)
-        stdout.pipe(f)
+        // stderr.pipe(error_stream)
+        gm(stdout)
+        .append(append_background)
+        .stream(function(err, stdout, stderr) {
+            // stderr.pipe(error_stream)
+            gm(stdout)
+            .drawText(0, 15, 'Okupatsioonide Muuseum #' + exp_nr + '\n' + nimetus + '\nokupatsioon.entu.ee', 'south')
+            .stream(function(err, stdout, stderr) {
+                // stderr.pipe(error_stream)
+                var f = fs.createWriteStream(download_filename)
+                stdout.pipe(f)
+            })
+        })
     })
-
-        // .pipe(resize).stream().pipe(fs.createWriteStream(download_filename))
 
 }
 util.inherits(fetchFile, EventEmitter)
