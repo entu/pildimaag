91d8fa31084627c81a55fb54111bb49ecc733f22
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-05-30 08:58:31 +0300

    queue for parallel loading

---
+++ queue.js
@@ -0,0 +1,65 @@
+var util            = require('util')
+var EventEmitter    = require('events').EventEmitter
+
+var Queue = function Queue(limit) {
+    EventEmitter.call(this)
+    var increment = 0
+    var queue = []
+    var active = 0
+    next = function next() {
+        if (active < limit && queue.length > 0) {
+            var job = queue.shift()
+            active ++
+            console.log(Date().toString() + ' Start ' + job.name + ' from queue. Active/Queue size ' + active + '/' + queue.length)
+            job.job_function(job.job_data, function finalCB(err) {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                if (job.finished) {
+                    console.log(Date().toString() + ' Cant finish again ' + job.name + ' already finished!')
+                    return
+                }
+                job.finished = true
+                active --
+                console.log(Date().toString() + ' Finished ' + job.name + '.')
+                next()
+            })
+        } else if (active >= limit) {
+            console.log(Date().toString() + ' Active queue full. Enqueue ' + job.name + '. Active/Queue size ' + active + '/' + queue.length)
+        } else if (queue.length === 0) {
+            console.log(Date().toString() + ' queue empty...')
+        }
+    }
+    return {
+        add: function(name, jobData, jobFunction) {
+            increment ++
+            queue.push({'id':increment, 'name':name, 'job_data':jobData, 'job_function':jobFunction, 'finished':false})
+            console.log(Date().toString() + ' Adding ' + name + ' to queue position ' + queue.length)
+            next()
+        }
+    }
+}
+util.inherits(Queue, EventEmitter)
+
+
+module.exports = Queue
+
+//
+// Sample usage
+//
+// console.log('aa')
+// var q = new Queue(3)
+// for (var i = 1; i <= 15; i++) {
+//     var jobData = {
+//         'i': i,
+//         'message': 'working on ###' + i
+//     }
+//     q.add(jobData, function jobFunction(jobData, finishCB) {
+//         console.log(Date().toString() + ' job ' + jobData['message'] + ' sleeps for ' + jobData['i'] + 's')
+//         setTimeout(function() {
+//             console.log(Date().toString() + ' job ' + jobData['message'] + ' finished.')
+//             finishCB(null)
+//         }, jobData['i'] * 1000)
+//     })
+// }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var nomnom          = require('nomnom')
-var request         = require('request');
+var request         = require('request')
 var EventEmitter    = require('events').EventEmitter
 var fs              = require('fs')
 var util            = require('util')
@@ -13,7 +13,10 @@ var stream          = require('stream')
 var gm              = require('gm')
 
 var entulib         = require('./entulib.js')
+var queue           = require('./queue.js')
 var helper          = require('./helper.js')
+
+var q = queue(10)
 
 // console.log(process.argv)
 var pjson = require('./package.json')
@@ -39,14 +42,15 @@ opts.HOSTNAME = 'okupatsioon.entu.ee'
 HOME_DIR = path.dirname(process.argv[1])
 TEMP_DIR = path.resolve(HOME_DIR, 'temp')
 PAGE_SIZE_LIMIT = 5
-MAX_DOWNLOAD_COUNT = 10
+MAX_DOWNLOAD_COUNT = 15
 PIC_READ_ENTITY = 'eksponaat'
-PIC_READ_PROPERTY = 'photo'
-PIC_WRITE_PROPERTY = 'thumb'
+PIC_READ_PROPERTY = 'photo-orig'
+PIC_WRITE_PROPERTY = 'photo'
+//  thumb
 
 var EntuLib = entulib(opts.USER_ID, opts.API_KEY, opts.HOSTNAME)
 var connection_counter = 0
-var download_counter = 0
+// var download_counter = 0
 
 
 ENTU_URI = 'https://' + opts.HOSTNAME + '/'
@@ -56,26 +60,21 @@ ENTU_API_POST_FILE = ENTU_API + 'file/s3'
 
 var fetchNextPage = function fetchNextPage(page) {
 
-    connection_counter ++
+    // connection_counter ++
     EntuLib.findEntity(PIC_READ_ENTITY, '', PAGE_SIZE_LIMIT, page, function findEntityCB(err, result) {
-        connection_counter --
+        // connection_counter --
         if (err) {
             console.log('findEntityCB: Can\'t reach Entu', err, result)
-            process.exit(99)
+            setTimeout(function() { fetchNextPage(page) }, 10*1000)
         }
         else if (result.error !== undefined) {
             console.log (result.error, 'Failed to fetch from Entu.')
-            process.exit(99)
+            setTimeout(function() { fetchNextPage(page) }, 10*1000)
         } else {
-
-            // console.log('Fetched ' + result.result.length + '/' + result.count
-            //     + ' results on page ' + page + '/' + Math.ceil(result.count / PAGE_SIZE_LIMIT))
-            // console.log(result.result)
             result.result.forEach(function entityLoop(entity) {
-
-                connection_counter ++
+                // connection_counter ++
                 EntuLib.getEntity(entity.id, function getEntityCB(err, result) {
-                    connection_counter --
+                    // connection_counter --
                     // console.log('#:' + connection_counter)
                     if (err) {
                         console.log('getEntityCB: Can\'t reach Entu', err, result)
@@ -85,7 +84,7 @@ var fetchNextPage = function fetchNextPage(page) {
                         console.log(result.error, 'Failed to fetch from Entu.')
                         process.exit(99)
                     } else {
-                        // console.log(entity.id + ':', result.result.displayname, result.result.displayinfo)
+                        // console.log(entity.id + ':', result.result.properties)
                         var photo_property = result.result.properties[PIC_READ_PROPERTY]
                         var thumb_property = result.result.properties[PIC_WRITE_PROPERTY]
                         var code_value = result.result.properties['code'].values ? result.result.properties['code'].values[0].value : ''
@@ -97,21 +96,34 @@ var fetchNextPage = function fetchNextPage(page) {
                                     thumb_property.values.forEach(function thumbLoop(thumb_val) {
                                         if (thumb_val.value === photo_val.value) {
                                             thumb_is_present = true
+                                            // console.log(photo_property.values)
+                                            // console.log(thumb_property.values)
+                                            // process.exit(99)
                                         }
-                                        // console.log(photo_property.values)
-                                        // console.log(thumb_property.values)
-                                        // process.exit(99)
                                     })
                                 }
                                 if (thumb_is_present) {
                                     return
                                 }
                                 // console.log(entity.id + '/' + photo_val.id + '[' + photo_val.db_value + ']:', photo_val.value)
-                                var ff = new fetchFile(entity.id, photo_val.db_value, photo_val.value, code_value, nimetus_value)
-                                .on('error', function fileFetchError(err_msg, err_no) {
-                                    console.log('fetchFile error: ', err_msg, err_no)
-                                    process.exit(err_no)
+                                var jobData = {
+                                    'eid':          entity.id,
+                                    'photo_db_val': photo_val.db_value,
+                                    'photo_val':    photo_val.value,
+                                    'code_val':     code_value,
+                                    'nimetus_val':  nimetus_value
+                                }
+                                q.add('Download of ' + photo_val.db_value, jobData, function jobFunction(jobData, finalCB) {
+                                    fetchFile(jobData.eid, jobData.photo_db_val, jobData.photo_val, jobData.code_val, jobData.nimetus_val, finalCB)
                                 })
+                                // var ff = new fetchFile(entity.id, photo_val.db_value, photo_val.value, code_value, nimetus_value)
+                                // .on('error', function fileFetchError(err_msg, err_no) {
+                                //     console.log('fetchFile error: ', err_msg, err_no)
+                                //     process.exit(err_no)
+                                // })
+                                // .on('ready'), function fileFetchReady(result) {
+                                //     console.log('SUCCESS: ', result)
+                                // }
                             })
                         }
                     }
@@ -121,12 +133,12 @@ var fetchNextPage = function fetchNextPage(page) {
             if (PAGE_SIZE_LIMIT * page < result.count) {
                 // console.log('Currently connections: #' + connection_counter + '; downloads: #' + download_counter)
                 var fetchIfReady = function fetchIfReady(page) {
-                    if (download_counter < MAX_DOWNLOAD_COUNT) {
+                    // if (download_counter < MAX_DOWNLOAD_COUNT) {
                         fetchNextPage(page)
-                    } else {
-                        console.log('Postponing load of page #' + page  + '/' + Math.ceil(result.count / PAGE_SIZE_LIMIT), 'Active downloads: ' + download_counter)
-                        setTimeout(function() { fetchIfReady(page) }, 5*1000)
-                    }
+                    // } else {
+                        // console.log('Postponing load of page #' + page  + '/' + Math.ceil(result.count / PAGE_SIZE_LIMIT), 'Active downloads: ' + download_counter)
+                        // setTimeout(function() { fetchIfReady(page) }, 5*1000)
+                    // }
                 }
                 fetchIfReady(page + 1)
             } else {
@@ -140,30 +152,30 @@ var fetchNextPage = function fetchNextPage(page) {
     })
 }
 
-fetchNextPage(1900)
+fetchNextPage(3200)
 
 var MAX_DOWNLOAD_TIME = 60 * 60 // seconds
 var total_download_size = 0
 var bytes_downloaded = 0
-var decrementProcessCount = function decrementProcessCount() {
-    -- download_counter
-}
-var incrementProcessCount = function incrementProcessCount() {
-    ++ download_counter
-}
-var countLoadingProcesses = function countLoadingProcesses() {
-    return download_counter
-}
+// var decrementProcessCount = function decrementProcessCount() {
+//     -- download_counter
+// }
+// var incrementProcessCount = function incrementProcessCount() {
+//     ++ download_counter
+// }
+// var countLoadingProcesses = function countLoadingProcesses() {
+//     return download_counter
+// }
 
 var append_background = path.resolve(HOME_DIR, 'text_background.png')
 
-var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) {
-    incrementProcessCount()
+var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetus, finalCB) {
+    // incrementProcessCount()
     EventEmitter.call(this)
     var self = this
 
     var t = setTimeout(function() {
-        decrementProcessCount()
+        // decrementProcessCount()
         self.emit('error', 'Error: ' + fetch_uri + ' timed out!', 95)
     }, MAX_DOWNLOAD_TIME * 1000)
 
@@ -173,7 +185,7 @@ var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetu
     gm(request
         .get(fetch_uri)
         .on('error', function(err) {
-            decrementProcessCount()
+            // decrementProcessCount()
             console.log('WARNING: request: ' + fetch_uri , err)
             setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
             return
@@ -182,8 +194,8 @@ var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetu
             var filesize = response.headers['content-length']
             // console.log(response.headers)
             if (filesize === undefined) {
-                decrementProcessCount()
-                console.log('WARNING: filesize === undefined: ' + fetch_uri )
+                // decrementProcessCount()
+                console.log('WARNING: filesize === undefined: ' + fetch_uri  + '.')
                 // setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
                 return
             } else {
@@ -203,7 +215,7 @@ var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetu
                 if (response.statusCode === 200) {
                     // console.log('Finished: ' + fetch_uri + ' - ' + helper.bytesToSize(total_download_size) + ' - ' + helper.bytesToSize(bytes_downloaded) + ' = ' + helper.bytesToSize(total_download_size - bytes_downloaded) )
                 }
-                decrementProcessCount()
+                // decrementProcessCount()
                 clearTimeout(t)
             })
         })
@@ -218,28 +230,29 @@ var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetu
             .drawText(0, 15, 'Okupatsioonide Muuseum #' + exp_nr + '\n' + nimetus + '\nokupatsioon.entu.ee', 'south')
             .stream(function(err, stdout, stderr) {
                 if (err) {
-                    console.log('WARNING: bm.stream: ' + fetch_uri , err, 'Active downloads' + download_counter)
-                    setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
+                    console.log('WARNING: bm.stream: ' + fetch_uri , err)
+                    // setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
                     return
                 }
                 var f = fs.createWriteStream(download_filename)
                 stdout.pipe(f)
                 f.on('finish', function() {
                     if (f.bytesWritten === 0) {
-                        console.log('WARNING: f.bytesWritten === 0: ' + fetch_uri )
-                        setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
+                        console.log('WARNING: f.bytesWritten === 0: ' + fetch_uri  + '.')
+                        // setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
                         return
                     }
-                    // console.log('bytes written: ' + f.bytesWritten);
-                    incrementProcessCount()
+                    console.log('bytes written: ' + f.bytesWritten);
+                    // incrementProcessCount()
                     EntuLib.addFile(entity_id, PIC_READ_ENTITY + '-' + PIC_WRITE_PROPERTY, file_name, 'image/jpeg', f.bytesWritten, download_filename, function addFileCB(err, result) {
-                        decrementProcessCount()
+                        // decrementProcessCount()
                         if (err) {
                             console.log('WARNING: addFileCB: ' + fetch_uri , err, result)
                             setTimeout(function() { fetchFile(entity_id, file_id, file_name, exp_nr, nimetus) }, 10 * 1000)
                             return
                         }
-                        console.log('SUCCESS: ' + fetch_uri + ' ' + helper.bytesToSize(f.bytesWritten))
+                        console.log('SUCCESS: ' + fetch_uri + ' ' + helper.bytesToSize(f.bytesWritten) + '.')
+                        finalCB(null)
                     })
                 })
             })
