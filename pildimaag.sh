ab3bc0c40c6f8c21f5b6049d7f686e555767f866
Argo Roots <argo@roots.ee>
2015-09-23 19:11:59 +0300

    pildimaag.sh update

--- pildimaag.sh
+++ pildimaag.sh
@@ -3,20 +3,25 @@
 mkdir -p /data/pildimaag/code
 cd /data/pildimaag/code
 
-git clone https://github.com/mitselek/pildimaag.git ./
-git checkout master
+git clone -q https://github.com/mitselek/pildimaag.git ./
+git checkout -q master
 git pull
+printf "\n\n"
 
 version=`date +"%y%m%d.%H%M%S"`
-
 docker build -q -t pildimaag:$version ./ && docker tag -f pildimaag:$version pildimaag:latest
-docker kill pildimaag
+printf "\n\n"
+
+docker stop pildimaag
 docker rm pildimaag
 docker run -d \
     --name="pildimaag" \
     --restart="always" \
-    --memory="256m" \
+    --memory="512m" \
     --env="PM_ENTITY=" \
     --env="PM_KEY=" \
     --env="PM_NIGHT_MINUTES=420" \
     pildimaag:latest
+
+docker inspect -f "{{ .NetworkSettings.IPAddress }}" pildimaag
+printf "\n\n"
