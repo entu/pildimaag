bc2dc304e94b1575e96b25ad8976b195c3fd092b
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-01-25 17:01:52 +0200

    prepare concrete tasks for addition and removal

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 if(process.env.NEW_RELIC_LICENSE_KEY) require('newrelic')
 
 var async           = require('async')
+var path            = require('path')
+var debug           = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var runTask         = require('./taskrunner')
 
 async.each(require('./jobs.json'), function iterator(job, callback) {
-    console.log('Starting job "' + job.name + '"')
+    debug('Starting job "' + job.name + '"')
     var entuOptions = {
         entuUrl: job.entuUrl,
         user: job.apiUser,
         key: job.apiKey,
+        relaxBetweenPages: job.relaxBetween.pagesSeconds,
     }
     async.forever(function(next) {
+        debug('=== tick forever ' + job.name + '===')
         async.eachSeries(job.tasks, function iterator(task, callback) {
-            console.log(job.name, 'Starting task: "' + task.name + '"')
+            debug(job.name + ' starting task "' + task.name + '"')
             runTask(task, entuOptions)
             .then(function(result) {
-                console.log(job.name, result)
-                callback()
+                debug(job.name, result)
+                setTimeout(callback, job.relaxBetween.tasksSeconds * 1e3)
             })
         },
         function(err) {
             if (err) { return callback(err) }
-            setTimeout(next, job.sleepMinutes * 60e3)
+            setTimeout(next, job.relaxBetween.roundtripMinutes * 60e3)
         }
     )},
     function(err) {
@@ -30,7 +34,7 @@ async.each(require('./jobs.json'), function iterator(job, callback) {
 },
 function(err) {
     if (err) {
-        console.log('Pildimaag stopped.')
+        debug('Pildimaag stopped.')
         throw err
     }
 })
