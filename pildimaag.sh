b3bff0bcf3ce10944f8f623cc9c96737dc4ef237
Argo Roots <argo@roots.ee>
2016-02-09 10:10:06 +0200

    pildimaag.sh

--- pildimaag.sh
+++ pildimaag.sh
@@ -4,24 +4,24 @@ mkdir -p /data/pildimaag/code
 cd /data/pildimaag/code
 
 git clone -q https://github.com/mitselek/pildimaag.git ./
-git checkout -q master
+git checkout -q aysnc-promise
 git pull
-printf "\n\n"
 
+printf "\n\n"
 version=`date +"%y%m%d.%H%M%S"`
-docker build -q -t pildimaag:$version ./ && docker tag -f pildimaag:$version pildimaag:latest
-printf "\n\n"
+docker build --quiet --pull --tag=pildimaag:$version ./ && docker tag pildimaag:$version pildimaag:latest
 
+printf "\n\n"
 docker stop pildimaag
 docker rm pildimaag
 docker run -d \
     --name="pildimaag" \
     --restart="always" \
-    --memory="512m" \
+    --cpu-shares=512 \
+    --memory="1g" \
+    --env="NODE_ENV=production" \
+    --env="VERSION=$version" \
     --env="PM_ENTITY=" \
     --env="PM_KEY=" \
     --env="PM_NIGHT_MINUTES=420" \
     pildimaag:latest
-
-docker inspect -f "{{ .NetworkSettings.IPAddress }}" pildimaag
-printf "\n\n"
